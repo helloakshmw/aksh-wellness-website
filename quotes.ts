8beee9bdd2902378c8ac8bf8a5102629c93c608d
helloakshmw <hello.akshmw@gmail.com>
2026-09-11 14:29:47 +0530

    Create quotes.ts

---
+++ quotes.ts
@@ -0,0 +1,9 @@
+export const quotes = [
+  "You don’t have to figure everything out today.",
+  "It’s okay to pause.",
+  "You deserve a space where you can simply be.",
+  "One small step is still a step forward.",
+  "You can begin exactly where you are.",
+  "Some days are for moving forward. Some are simply for breathing.",
+  "You don’t have to carry everything alone.",
+] as const;
